51c1392c7812fd8fe7a106a5349d18a3c598b102
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-02 01:38:31 -0500

    learned the order of constructor render compDidMount integrated an API to populate the array

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
-import React, {Component} from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import { robots } from './robots';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
 import './App.css';
 
-const state = {
-    robots: robots,
-    searchfield: ''
-}
-
-
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            robots: robots,
-            searchfield: ''
-        }
-    }
+	constructor() {
+		super();
+		this.state = {
+			robots: [],
+			searchfield: '',
+		};
+	}
 
-    onSearchChange = (event) => {
-        this.setState({
-            searchfield: event.target.value
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then((users) => this.setState({robots: users}));
+	}
+
+	onSearchChange = (event) => {
+		this.setState({
+			searchfield: event.target.value,
+		});
+	};
+
+	render() {
+		const filteredRobots = this.state.robots.filter((robots) => {
+			return robots.name
+				.toLowerCase()
+				.includes(this.state.searchfield.toLowerCase());
         });
-    }
-    
-    render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        });
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots = { filteredRobots } />
-            </div>
-        );
-    }    
+
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
+	}
 }
 
 export default App;
